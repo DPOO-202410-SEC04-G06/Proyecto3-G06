45bfb1aad98fdb7f68db46804570f218574ddb2e
adrianvelasquezs <a.velasquezs@uniandes.edu.co>
2024-05-04 20:23:55 -0500

    Modificaciones Consola Empleado

--- Entrega1/consola/ConsolaEmpleado.java
+++ Entrega1/consola/ConsolaEmpleado.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import controlador.ControladorGaleria;
-import piezas.Pieza;
-import usuarios.Empleado;
+import galeria.*;
+import piezas.*;
+import usuarios.*;
 
 public abstract class ConsolaEmpleado extends ConsolaBasica 
 {
@@ -40,7 +41,75 @@ public abstract class ConsolaEmpleado extends ConsolaBasica
 	
 	// ############################################ Metodos
 	
+	/**
+	 * Actualiza el estado de una pieza en la galeria. El empleado corriente solamente podra actualizar 
+	 * el estado a bodega o a exposicion
+	 */
+	protected void actualizarEstadoPieza()
+	{
+		int newState;
+		
+		String nombrePieza = this.pedirCadenaAlUsuario("Digite el nombre de la pieza que desea actualizar");
+		
+		String[] opcionesEstado = { "Bodega", "Exponer" };
+		
+		int iUpdateState = this.mostrarMenu( "Elija el nuevo estado", opcionesEstado);
+		
+		if ( iUpdateState == 1 )
+		{
+			newState = Inventario.BODEGA;
+		}
+		else
+		{
+			newState = Inventario.EXPUESTA;
+		}
+		
+		usuario.actualizarEstadoPieza(nombrePieza, newState, controladorGaleria.galeria.getInventarioPiezas());
+		correrConsola();
+	}
 	
+	/**
+	 * Cambia el propietario de una pieza actualmente en la galeria
+	 */
+	protected void cambiarPropietarioPieza()
+	{
+		String nombreUsuario = this.pedirCadenaAlUsuario("Digite el nombre del nuevo propietario");
+		UsuarioCorriente propietario = controladorGaleria.galeria.buscarUsuarioCorrienteUsername(nombreUsuario);
+		
+		if ( propietario == null )
+		{
+			System.out.println( "No se encontro el usuario, intente de nuevo");
+			correrConsola();
+		}
+		
+		String nombrePieza = this.pedirCadenaAlUsuario("Digite el nombre de la pieza");
+		Pieza pieza = controladorGaleria.galeria.consultarPiezaGaleria(nombrePieza);
+		
+		if ( pieza == null )
+		{
+			System.out.println( "No se encontro el la pieza, intente de nuevo");
+			correrConsola();
+		}
+		
+		usuario.cambiarPropietarioPieza(nombrePieza, pieza, propietario);
+		correrConsola();
+	}
+	
+	/**
+	 * Genera una nueva transaccion en el sistema
+	 */
+	protected void nuevaTransaccion()
+	{
+		//TODO
+	}
+	
+	/**
+	 * Busca una transaccion en el sistema
+	 */
+	protected void buscarTransaccion()
+	{
+		//TODO
+	}
 	
 	/**
 	 * Consulta una pieza
